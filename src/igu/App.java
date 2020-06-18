eec1fd32bbf7fb966d7ea31cf49ac8fb46d41698
Sullon <asullom@upeu.edu.pe>
2020-06-18 10:33:49 -0500

    first commit

---
+++ src/igu/App.java
@@ -0,0 +1,17 @@
+package igu;
+
+/**
+ *
+ * @author Asullom
+ */
+public class App {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new Main1Frame().setVisible(true);
+    }
+    
+}
